8f4db83f0f68b5a0da2f97480ab26349c5adea3c
imadhui <imadhui.cs@gmail.com>
2022-09-23 12:50:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,37 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Word!'));
+let date_ob = new Date();
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+
+// prints date in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+
+// prints time in HH:MM format
+let time = hours + ":" + minutes
+
+app.get('/', (req, res) => res.send(time));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
