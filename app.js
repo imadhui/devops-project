e34ef74666b40150aeaff5586a4688585d08117e
imadhui <imadhui.cs@gmail.com>
2022-09-23 13:20:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,37 +2,24 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-let date_ob = new Date();
-
-// current date
-// adjust 0 before single digit date
-let date = ("0" + date_ob.getDate()).slice(-2);
-
-// current month
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-// current year
-let year = date_ob.getFullYear();
-
-// current hours
-let hours = date_ob.getHours();
-
-// current minutes
-let minutes = date_ob.getMinutes();
-
-// current seconds
-let seconds = date_ob.getSeconds();
-
-// prints date in YYYY-MM-DD format
-console.log(year + "-" + month + "-" + date);
+// let date_ob = new Date();
+// let date = ("0" + date_ob.getDate()).slice(-2);
+// let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+// let year = date_ob.getFullYear();
+// let hours = date_ob.getHours();
+// let minutes = date_ob.getMinutes();
+// let seconds = date_ob.getSeconds();
 
 // prints date & time in YYYY-MM-DD HH:MM:SS format
-console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-
+// console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 // prints time in HH:MM format
-let time = hours + ":" + minutes
+// let time = hours + ":" + minutes
 
-app.get('/', (req, res) => res.send(time));
+function style_wrapper (text) {
+    return "<center style=\"font-size: 80px\">" + "<h1>" + text + "</h1>"  + "</center>"
+}
+
+app.get('/', (req, res) => res.send(style_wrapper("Hello World!")));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
